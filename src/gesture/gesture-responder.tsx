a63f5d52812e32866c1d02240b963dd0f1e740c5
Aliaksei Petrusevich <effectwaater@gmail.com>
2021-09-25 18:38:20 +0300

    [fix] fixed android bug(draw on map)

--- src/gesture/gesture-responder.tsx
+++ src/gesture/gesture-responder.tsx
@@ -2,66 +2,45 @@ import React, { FC, useRef } from 'react';
 import { PanResponder, StyleSheet, View } from 'react-native';
 import type { TouchPoint } from '../types';
 import type { IGestureProps } from './types';
-/**
- * PanResponder & native event descriptions
- * NATIVE_EVENT
- * - changedTouches - Array of all touch events that have changed since the last event
- * - identifier - The ID of the touch
- * - locationX - The X position of the touch, relative to the element
- * - locationY - The Y position of the touch, relative to the element
- * - pageX - The X position of the touch, relative to the root element
- * - pageY - The Y position of the touch, relative to the root element
- * - target - The node id of the element receiving the touch event
- * - timestamp - A time identifier for the touch, useful for velocity calculation
- * - touches - Array of all current touches on the screen
- *
- * GESTURE_STATE
- * - stateID - ID of the gestureState- persisted as long as there at least one touch on screen
- * - moveX - the latest screen coordinates of the recently-moved touch
- * - moveY - the latest screen coordinates of the recently-moved touch
- * - x0 - the screen coordinates of the responder grant
- * - y0 - the screen coordinates of the responder grant
- * - dx - accumulated distance of the gesture since the touch started
- * - dy - accumulated distance of the gesture since the touch started
- * - vx - current velocity of the gesture
- * - vy - current velocity of the gesture
- * - numberActiveTouches - Number of touches currently on screen
- */
 
 const GestureHandler: FC<IGestureProps> = ({
-  onEndTouchEvents,
-  onStartTouchEvents,
-  onChangeTouchEvents,
+    onEndTouchEvents,
+    onStartTouchEvents,
+    onChangeTouchEvents,
 }) => {
-  const pathRef = useRef<TouchPoint[]>([]);
+    const pathRef = useRef<TouchPoint[]>([]);
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onStartShouldSetPanResponderCapture: () => true,
-      onMoveShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponderCapture: () => true,
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onStartShouldSetPanResponderCapture: () => true,
+        onMoveShouldSetPanResponder: () => true,
+        onMoveShouldSetPanResponderCapture: () => true,
 
-      onPanResponderGrant: (e, gestureState) => {
-        pathRef.current = [];
-        onStartTouchEvents && onStartTouchEvents(e, gestureState);
-      },
-      onPanResponderMove: (event) => {
-        pathRef.current.push({
-          x: event.nativeEvent.locationX,
-          y: event.nativeEvent.locationY,
-        });
-        onChangeTouchEvents([...pathRef.current]);
-      },
-      onPanResponderRelease: () => {
-        const points = [...pathRef.current];
-        onChangeTouchEvents(points);
-        onEndTouchEvents && onEndTouchEvents(points);
-      },
-    })
-  ).current;
+        onPanResponderGrant: (e, gestureState) => {
+            pathRef.current = [];
+            onStartTouchEvents?.(e, gestureState);
+        },
+        onPanResponderMove: (event) => {
+            pathRef.current.push({
+                x: event.nativeEvent.locationX,
+                y: event.nativeEvent.locationY,
+            });
+            onChangeTouchEvents?.([...pathRef.current]);
+        },
+        onPanResponderRelease: () => {
+            const points = [...pathRef.current];
+            onChangeTouchEvents(points);
 
-  return <View style={StyleSheet.absoluteFill} {...panResponder.panHandlers} />;
+            onEndTouchEvents?.(points);
+        },
+    });
+
+    return (
+        <View
+            style={{ ...StyleSheet.absoluteFill, ...{ backgroundColor: 'transparent' } }}
+            {...panResponder.panHandlers}
+        />
+    );
 };
 
 export default GestureHandler;
--- src/polygon/polygon.tsx
+++ src/polygon/polygon.tsx
@@ -3,53 +3,53 @@ import { Svg, Defs, Rect, Mask, Polygon } from 'react-native-svg';
 import React, { FC } from 'react';
 import type { IPolygonProps } from './types';
 const MapPolygon: FC<IPolygonProps> = ({
-  path,
-  fillOverlay,
-  widthOverlayLine,
-  colorWidthOverlayLine,
-  backgroundOverlayPolygon,
+    path,
+    fillOverlay,
+    widthOverlayLine,
+    colorWidthOverlayLine,
+    backgroundOverlayPolygon,
 }) => {
-  const { width, height } = useWindowDimensions();
+    const { width, height } = useWindowDimensions();
 
-  return (
-    <Svg
-      height={height}
-      width={width}
-      viewBox={`0 0 ${width} ${height}`}
-      style={[
-        styles.container,
-        {
-          backgroundColor: backgroundOverlayPolygon,
-        },
-      ]}
-    >
-      <Defs>
-        <Mask id="mask" x="0" y="0" height="100%" width="100%">
-          <Rect height="100%" width="100%" fill="#fff" />
-          <Polygon
-            points={path}
-            fill={fillOverlay}
-            stroke={colorWidthOverlayLine}
-            strokeWidth={widthOverlayLine}
-          />
-        </Mask>
-      </Defs>
-      <Rect
-        height="100%"
-        width="100%"
-        fill="rgba(0, 0, 0, 0.7)"
-        mask="url(#mask)"
-        fill-opacity="0"
-      />
-    </Svg>
-  );
+    return (
+        <Svg
+            height={height}
+            width={width}
+            viewBox={`0 0 ${width} ${height}`}
+            style={[
+                styles.container,
+                {
+                    backgroundColor: backgroundOverlayPolygon,
+                },
+            ]}
+        >
+            <Defs>
+                <Mask id="mask" x="0" y="0" height="100%" width="100%">
+                    <Rect height="100%" width="100%" fill="#fff" />
+                    <Polygon
+                        points={path}
+                        fill={fillOverlay}
+                        stroke={colorWidthOverlayLine}
+                        strokeWidth={widthOverlayLine}
+                    />
+                </Mask>
+            </Defs>
+            <Rect
+                height="100%"
+                width="100%"
+                fill="rgba(0, 0, 0, 0.7)"
+                mask="url(#mask)"
+                fill-opacity="0"
+            />
+        </Svg>
+    );
 };
 
 export default MapPolygon;
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    zIndex: 4,
-  },
+    container: {
+        position: 'absolute',
+        zIndex: 4,
+    },
 });
