c96bda1b64536144fd5a8cb59af843d31b5c8b21
golnoosh.yazdani <golnoosh.yazdani@wahi.com>
2023-06-15 22:21:34 -0400

    feat: add zIndex to gesture-responder for Android issue fix

---
+++ src/components/draw-control/gesture-responder.style.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  gestureResponder: {
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    zIndex: 1,
+  },
+});
